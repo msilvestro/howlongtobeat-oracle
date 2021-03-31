b231568a6c5b37a4f59896b587643e249a179c3a
Matteo Silvestro <matteosilvestro@live.it>
2021-04-01 00:31:56 +0200

    Add support to show only recently updated games

--- howlongtobeat_oracle/main.py
+++ howlongtobeat_oracle/main.py
@@ -7,8 +7,17 @@ from howlongtobeat_oracle.website import HowLongToBeatWebsite, SortBy
 
 class HowLongToBeatOracle:
     @staticmethod
-    def get(page: Optional[int] = None, sort_by: SortBy = SortBy.most_popular):
-        html = HowLongToBeatWebsite.search_results(page=page, sort_by=sort_by)
+    def get(
+        page: Optional[int] = None,
+        sort_by: SortBy = SortBy.most_popular,
+        only_recently_updated=False,
+    ):
+        query_string = "recently updated" if only_recently_updated else None
+        html = HowLongToBeatWebsite.search_results(
+            query_string=query_string,
+            page=page,
+            sort_by=sort_by,
+        )
         result = HowLongToBeatParser.parse_game_list(html)
         return [HowLongToBeatGame(game) for game in result["data"]], result["pages"][
             "total_pages"
