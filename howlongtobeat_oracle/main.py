1996ab847f4b786663ad67d983c2d175931fd27c
Matteo Silvestro <matteosilvestro@live.it>
2021-03-31 23:19:18 +0200

    Add sort by

--- howlongtobeat_oracle/main.py
+++ howlongtobeat_oracle/main.py
@@ -1,9 +1,11 @@
+from typing import Optional
+
 from howlongtobeat_oracle.parser import HowLongToBeatParser
-from howlongtobeat_oracle.website import HowLongToBeatWebsite
+from howlongtobeat_oracle.website import HowLongToBeatWebsite, SortBy
 
 
 class HowLongToBeatOracle:
     @staticmethod
-    def get():
-        html = HowLongToBeatWebsite.search_results()
+    def get(page: Optional[int] = None, sort_by: SortBy = SortBy.most_popular):
+        html = HowLongToBeatWebsite.search_results(page=page, sort_by=sort_by)
         return HowLongToBeatParser.parse_game_list(html)
