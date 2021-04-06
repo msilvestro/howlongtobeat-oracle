fc2650076c9c47a6ec92c1c252ce7a59d2d7b316
Matteo Silvestro <matteosilvestro@live.it>
2021-04-06 18:56:27 +0200

    Handle special queries such as recently updated or added

--- hltb_oracle/main.py
+++ hltb_oracle/main.py
@@ -2,7 +2,7 @@ from typing import Optional
 
 from hltb_oracle.models import HowLongToBeatPage
 from hltb_oracle.parser import HowLongToBeatParser
-from hltb_oracle.website import HowLongToBeatWebsite, SortBy
+from hltb_oracle.website import HowLongToBeatWebsite, SortBy, SpecialQuery
 
 
 class HowLongToBeatOracle:
@@ -11,9 +11,9 @@ class HowLongToBeatOracle:
         search_query: Optional[str] = None,
         page: Optional[int] = None,
         sort_by: SortBy = SortBy.most_popular,
-        only_recently_updated=False,
+        special_query: SpecialQuery = None,
     ):
-        query_string = "recently updated" if only_recently_updated else search_query
+        query_string = special_query.value if special_query else search_query
         html = HowLongToBeatWebsite.search_results(
             query_string=query_string,
             page=page,
